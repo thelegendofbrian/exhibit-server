a8809176792eb49e3665b8679e4109ea40ef0249
Travis Bruce <travis.bruce1994@gmail.com>
2019-11-29 11:53:05 -0600

    schedule refactoring

--- src/main/kotlin/minepop/exhibit/schedule/Schedule.kt
+++ src/main/kotlin/minepop/exhibit/schedule/Schedule.kt
@@ -17,10 +17,34 @@ abstract class Schedule(var groupMemberId: Long, var startDate: Date) {
      * @param end The end date for the period of stats. Usually is the date that the group member is checking in.
      */
     abstract fun calculateStatsUpdate(stats: ScheduleStatsUpdate, start: LocalDate, end: LocalDate)
+
+    fun iterate(scheduleStart: LocalDate? = null, iterateStart: LocalDate? = null, iterateEnd: LocalDate, forEach: (date: LocalDate) -> Unit) {
+        val localStartDate = startDate.toLocalDate()
+        val actualStart = if (scheduleStart == null || localStartDate > scheduleStart) localStartDate else scheduleStart
+        val actualIterateStart = iterateStart ?: actualStart
+        iterate0(actualStart, actualIterateStart, iterateEnd, forEach)
+    }
+
+    abstract fun iterate0(scheduleStart: LocalDate, start: LocalDate, end: LocalDate, forEach: (date: LocalDate) -> Unit)
 }
 
 class WeeklySchedule(groupMemberId: Long, startDate: Date) : Schedule(groupMemberId, startDate) {
     var days: List<Int> = mutableListOf()
+
+    override fun iterate0(
+        scheduleStart: LocalDate,
+        start: LocalDate,
+        end: LocalDate,
+        forEach: (date: LocalDate) -> Unit
+    ) {
+        var weeklyDay = scheduleStart
+        while (weeklyDay < end) {
+            if (days.contains(weeklyDay.dayOfWeek.value)) {
+                forEach(weeklyDay)
+            }
+            weeklyDay = weeklyDay.plusDays(1)
+        }
+    }
 
     override fun calculateStatsUpdate(stats: ScheduleStatsUpdate, start: LocalDate, end: LocalDate) {
         /*
@@ -32,14 +56,8 @@ class WeeklySchedule(groupMemberId: Long, startDate: Date) : Schedule(groupMembe
             Step forward from the last scheduled check in one day at a time until today.
             If the day is a scheduled check in, then it was a missed check in.
          */
-        val scheduleStart = startDate.toLocalDate().minusDays(1)
-        var weeklyDay = if (start > scheduleStart) start else scheduleStart
-        weeklyDay = weeklyDay.plusDays(1)
-        while (weeklyDay < end) {
-            if (days.contains(weeklyDay.dayOfWeek.value)) {
-                stats.missedCheckins++
-            }
-            weeklyDay = weeklyDay.plusDays(1)
+        iterate(scheduleStart = start.plusDays(1), iterateEnd = end) {
+            stats.missedCheckins++
         }
     }
 }
@@ -47,22 +65,35 @@ class WeeklySchedule(groupMemberId: Long, startDate: Date) : Schedule(groupMembe
 class IntervalSchedule(groupMemberId: Long, startDate: Date) : Schedule(groupMemberId, startDate) {
     var days: Int? = null
 
+    override fun iterate0(
+        scheduleStart: LocalDate,
+        start: LocalDate,
+        end: LocalDate,
+        forEach: (date: LocalDate) -> Unit
+    ) {
+        var intervalDay = scheduleStart
+        val interval = days!!.toLong()
+        while (intervalDay < end) {
+            intervalDay = intervalDay.plusDays(interval)
+            forEach(intervalDay)
+        }
+    }
+
     override fun calculateStatsUpdate(stats: ScheduleStatsUpdate, start: LocalDate, end: LocalDate) {
         /*
             This could perform poorly if the start date was a long time ago.
          */
-        val scheduleStart = startDate.toLocalDate()
-        var intervalDay = if (start > scheduleStart) start else scheduleStart
-        val interval = days!!.toLong()
+        var intervalDay: LocalDate? = null
         /*
             Iterate the schedule forward through its scheduled days until today.
             For each iteration, there is expected to be 1 check in.
             So for each iteration past the first, there is a missed check in.
          */
-        while (intervalDay < end) {
-            intervalDay = intervalDay.plusDays(interval)
+        iterate(scheduleStart = start, iterateEnd = end) {
             stats.missedCheckins++
+            intervalDay = it
         }
+
         /*
             If the current check in does not much the expected next check in, then this is a bonus check in.
          */
